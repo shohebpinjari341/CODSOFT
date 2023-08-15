1343e60c47be4250aeef86da427d10adfd4213f6
shohebpinjari341 <138284138+shohebpinjari341@users.noreply.github.com>
2023-08-15 18:05:19 +0530

    Add files via upload

---
+++ CodeSoft Internship/AdressBookSystem.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Contact {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Contact(String name, String phoneNumber, String emailAddress) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Phone Number: " + phoneNumber + ", Email: " + emailAddress;
+    }
+}
+
+class AddressBook {
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public Contact findContactByName(String name) {
+        for (Contact contact : contacts) {
+            if (contact.getName().equalsIgnoreCase(name)) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    public void displayAllContacts() {
+        System.out.println("Contact List:");
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+}
+
+public class AdressBookSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+
+        while (true) {
+            System.out.println("\nOptions:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Search Contact");
+            System.out.println("3. Display All Contacts");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter contact name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter phone number: ");
+                    String phoneNumber = scanner.nextLine();
+                    System.out.print("Enter email address: ");
+                    String emailAddress = scanner.nextLine();
+                    Contact newContact = new Contact(name, phoneNumber, emailAddress);
+                    addressBook.addContact(newContact);
+                    System.out.println("Contact added successfully.");
+                    break;
+                case 2:
+                    System.out.print("Enter contact name to search: ");
+                    String searchName = scanner.nextLine();
+                    Contact foundContact = addressBook.findContactByName(searchName);
+                    if (foundContact != null) {
+                        System.out.println("Contact found:");
+                        System.out.println(foundContact);
+                    } else {
+                        System.out.println("Contact not found.");
+                    }
+                    break;
+                case 3:
+                    addressBook.displayAllContacts();
+                    break;
+                case 4:
+                    System.out.println("Exiting the application.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+        }
+    }
+}
